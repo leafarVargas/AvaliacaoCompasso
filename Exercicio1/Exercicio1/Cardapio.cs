dc1a75004f1da71919fe3ffcb8fc16f441285cd1
Rafael Vargas <rafaelsilveirav@icloud.com>
2021-09-19 22:02:30 -0300

    teste

---
+++ Exercicio1/Exercicio1/Cardapio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    public class Cardapio
+    {
+        public static List<Produto> cardapioLista()
+        {
+            var cardapioList = new List<Produto>();
+
+            var cachorroQuente = new Produto(100, "Cachorro Quente", 5.70);
+            cardapioList.Add(cachorroQuente);
+
+            var xCompleto = new Produto(101, "X - Completo", 18.30);
+            cardapioList.Add(xCompleto);
+
+            var xSalada = new Produto(102, "X - Salada", 16.50);
+            cardapioList.Add(xSalada);
+
+            var hamburguer = new Produto(103, "Hamburguer", 22.40);
+            cardapioList.Add(hamburguer);
+
+            var cocaCola = new Produto(104, "Coca 2L     ", 10.00);
+            cardapioList.Add(cocaCola);
+
+            var refri = new Produto(105, "Refrigerante", 1.0);
+            cardapioList.Add(refri);
+
+            return cardapioList;
+        }
+
+        
+    }
+
+}
---
+++ Exercicio1/Exercicio1/Restaurante.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Exercicio1
+{
+    public class Restaurante : Cardapio
+    {
+
+        static void Main(string[] args)
+        {
+
+            bool exibeMenu = true;
+            while (exibeMenu)
+            {
+                exibeMenu = Menu();
+            }
+
+            int[] mesas = { 1, 2, 3, 4 };
+            mesas[0] = 1;
+            mesas[1] = 2;
+            mesas[2] = 3;
+            mesas[3] = 4;
+
+        }
+        
+        private static bool Menu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.Write("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ PEDIDOS ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+            Console.WriteLine(" ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("╔═════════════════MENU DE OPÇÕES════════════════╗    ");
+            Console.WriteLine("║ 1 EFETUAR PEDIDO                              ║    ");
+            Console.WriteLine("║ 2 SAIR                                        ║    ");
+            Console.WriteLine("╚═══════════════════════════════════════════════╝    ");
+            Console.WriteLine(" ");
+            Console.Write("DIGITE UMA OPÇÃO : ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+
+                    Console.WriteLine("CODIGO \t" + "PRODUTO \t" + "PREÇO \t");
+                    MostrarCardapio();
+                    Console.WriteLine(" ");
+                    Console.WriteLine("DIGITE O NÚMERO DA MESA QUE DESEJA ESCOLHER ( ENTRE 1 E 4): ");
+                    
+                    int opc = int.Parse(Console.ReadLine());
+                    bool exibecardapio = true;
+
+                    var pedidos = new List<Pedido>();
+
+
+                    while (exibecardapio)
+                        {
+                            Console.WriteLine("ESCOLHA SEU PRODUTO: (Digite 999 para concluir) ");
+                            int prod = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("DIGITE A QUANTIDADE: ");
+                            int quant = int.Parse(Console.ReadLine());
+
+                            
+                            var item = new Pedido(opc, quant, prod);  
+
+                            pedidos.Add(item);
+
+                            Console.WriteLine(pedidos);
+                            Console.ReadLine();
+
+                            if (prod == 999)
+                            {
+                                return exibecardapio = false;
+                            }
+                    }
+                    return true;
+                
+                case "2":
+
+                    return false;
+                
+                default:
+                    return true;
+            }
+        }
+        private static void MostrarCardapio()
+        {
+            foreach (var cardapioList in cardapioLista())
+            {
+                Console.WriteLine(cardapioList.codigoProd + "\t" + cardapioList.descricaoProd + "\t R$ " + cardapioList.valorProd + "\t");
+            }
+        }
+    }
+}
